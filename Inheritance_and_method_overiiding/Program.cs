8c9b1be802c91b91549cc125f625f06b2b245f90
Clifford A. Boateng <aboateng_clifford@st.ug.edu.gh>
2024-06-16 14:06:09 +0000

    inheritance and method overloading implementation complete

--- Inheritance_and_method_overiiding/Program.cs
+++ Inheritance_and_method_overiiding/Program.cs
@@ -1,3 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Media;
 
-Console.WriteLine("Hello, World!");
+namespace OOPConcept
+{
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Generic sound goes here!");
+        }
+        
+    }
+
+    class Dog : Animal
+    { 
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Animal genAnimal = new Animal();
+
+            Dog dog = new Dog();
+
+            Cat cat = new Cat();
+            
+            cat.MakeSound();
+            dog.MakeSound();
+            genAnimal.MakeSound();
+        }
+    }
+}
+
--- Inheritance_and_method_overiiding/obj/Debug/net8.0/Inheritance_and_method_overiiding.AssemblyInfo.cs
+++ Inheritance_and_method_overiiding/obj/Debug/net8.0/Inheritance_and_method_overiiding.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inheritance_and_method_overiiding")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c76cb717df647f801832c7716892a9f1ddd6e49a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inheritance_and_method_overiiding")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inheritance_and_method_overiiding")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
