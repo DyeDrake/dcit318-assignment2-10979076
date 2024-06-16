cc56acf22d6f82204795fa3f4a67d4717bca7293
Clifford A. Boateng <aboateng_clifford@st.ug.edu.gh>
2024-06-16 14:42:42 +0000

    Abstract class and method for shape implementation complete

---
+++ Abstract_class_and-methods/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ShapeExample
+{
+    abstract class Shape
+    {
+        public abstract double GetArea();
+
+    }
+
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        // Implement GetArea
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    class Rectagle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectagle(double width, double height)
+        {
+            Height = height;
+            Width = width;
+        }
+        
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+    }
+
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Circle circle = new Circle(12.034);
+            Rectagle rectagle = new Rectagle(7.48, 20.76);
+            
+            Console.WriteLine($"Area of the circle is {circle.GetArea()} with radius {circle.Radius}");
+            Console.WriteLine($"Area of the Rectangle is {rectagle.GetArea()} with respective height and width" +
+                              $" {rectagle.Height}, {rectagle.Width}");
+        }
+    }
+}
