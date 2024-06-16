ba54378314260d9f213a8560a6e148543be60bf7
Clifford A. Boateng <aboateng_clifford@st.ug.edu.gh>
2024-06-16 15:00:01 +0000

    interface implementation on IMovable and vehicles completed

---
+++ Interface_and_implementation_car/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+interface IMovable
+{
+    void Move();
+}
+
+class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+class Bycicle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Bycicle is moving");
+    }
+}
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        IMovable car = new Car();
+        IMovable bycicle = new Bycicle();
+
+        car.Move();
+        bycicle.Move();
+
+        Console.ReadLine(); 
+    }
+}
